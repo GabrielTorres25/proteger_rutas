20781c53f15023d31c8686261343723b6fcaa2e5
GabrielTorres25 <gabicjs87@gmail.com>
2026-05-07 18:35:22 -0300

    error corregido
    se corrigio un error en la escritura que rompia el codigo

--- src/pages/client/home/home.ts
+++ src/pages/client/home/home.ts
@@ -1,19 +1,12 @@
-import { checkAuhtUser, logout } from "../../../utils/auth";
+import { checkAuthUser, logout } from "../../../utils/auth";
 
-const buttonLogout = document.getElementById(
-  "logoutButton"
-) as HTMLButtonElement;
+const buttonLogout = document.getElementById("logoutButton") as HTMLButtonElement;
 buttonLogout?.addEventListener("click", () => {
   logout();
 });
 
-
-const initPage = () => {
-  console.log("inicio de pagina");
-  checkAuhtUser(
+checkAuthUser(
     "/src/pages/auth/login/login.html",
     "/src/pages/admin/home/home.html",
-    "client"
-  );
-};
-initPage();
+    "client");
+
